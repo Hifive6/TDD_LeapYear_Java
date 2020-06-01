29fff5a0af528ae4138460f11c72d85acca2a086
David Juarez <djuar05@gmail.com>
2020-06-01 14:43:33 -0400

    create and test method for year with 00 at the end

--- LeapYear.java
+++ LeapYear.java
@@ -3,4 +3,8 @@ public class LeapYear {
     public boolean isDivisibleBy4(int number){
         return number % 4 == 0;
     }
+
+    public boolean isDivisibleBy100(int number){
+        return number % 100 == 0;
+    }
 }
--- LeapYearTest.java
+++ LeapYearTest.java
@@ -10,4 +10,10 @@ public class LeapYearTest {
         LeapYear leap = new LeapYear();
         Assert.assertEquals(true, leap.isDivisibleBy4(1996));
     }
+
+    @Test
+    public final void isYearDivisibleBy100(){
+        LeapYear leap = new LeapYear();
+        Assert.assertEquals(true, leap.isDivisibleBy100(2000));
+    }
 }
