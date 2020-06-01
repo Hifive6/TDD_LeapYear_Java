4ccaea070b6c6c59af9da8bced1829c541275ff1
David Juarez <djuar05@gmail.com>
2020-06-01 14:27:23 -0400

    test if number of year is divisible by 4 failed

--- LeapYearTest.java
+++ LeapYearTest.java
@@ -1,6 +1,15 @@
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Assert;
 
 public class LeapYearTest {
     
+
+    @Test
+    public final void isYearDivisibleBy4(){
+        LeapYear leap = new LeapYear();
+        assertEquals(499, leap.divisibleBy4(1996));
+    }
 }
